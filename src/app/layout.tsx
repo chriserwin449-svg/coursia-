c267842f2eb0433eb066d05de9b18f12936bd59a
Z User <z@container>
2026-07-28 20:26:09 +0000

    8f12402a-1fab-42d6-85a6-54cf23d721c2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -156,6 +156,21 @@ export default function RootLayout({
         <Toaster />
         <Analytics />
         <SpeedInsights />
+
+        {/* Tawk.to Live Chat */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a690e0d64a1cb1d4e015ac7/1jul5tpf4';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`,
+          }}
+        />
       </body>
     </html>
   );
